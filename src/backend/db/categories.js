2b6afa217bbb479b515b6936961e4c05df2c768e
Varun Verma <28varun28pp@gmail.com>
2022-05-17 12:57:11 +0530

    add: data for videos

--- src/backend/db/categories.js
+++ src/backend/db/categories.js
@@ -8,20 +8,30 @@ import { v4 as uuid } from "uuid";
 export const categories = [
   {
     _id: uuid(),
-    categoryName: "Computer Programming",
-    description:
-      "Computer programming is the process of designing and building an executable computer program to accomplish a specific computing",
+    categoryName: "science-experiments",
+    categoryTitle: "Science Experiments",
+    description: "Amazing tricks and fun experiments you can repeat at home",
+    categoryImg: "https://firebasestorage.googleapis.com/v0/b/video-library-image-data.appspot.com/o/Science-craft%2Fscience1.jpg?alt=media&token=5d86ea5a-51ad-4633-ae9f-14fc5309ccff" 
   },
   {
     _id: uuid(),
-    categoryName: "Frontend Development",
-    description:
-      "Front-end web development, also known as client-side development is the practice of producing HTML, CSS and JavaScript for a website or Web Application",
+    categoryName: "craft",
+    categoryTitle: "Craft",
+    description: "HACKS EVERYBODY IS GOING TO LOVE",
+    categoryImg: "https://firebasestorage.googleapis.com/v0/b/video-library-image-data.appspot.com/o/craft%2Fcraft1.webp?alt=media&token=e83135d6-2cd4-4ad6-a008-e118c3a773d8" 
   },
   {
     _id: uuid(),
-    categoryName: "Backend Development",
-    description:
-      "Backend Development is also known as server-side development. It is everything that the users don't see and contains behind-the-scenes activities that occur when performing any action on a website.",
+    categoryName: "smart-parenting-hacks",
+    categoryTitle: "Smart Parenting Hacks",
+    description: "COOL WAYS TO ENTERTAIN YOURSELF",
+    categoryImg: "https://firebasestorage.googleapis.com/v0/b/video-library-image-data.appspot.com/o/smart-parenting-hacks%2Fparenting1.webp?alt=media&token=4478835a-95c6-4b0d-898a-82d2e6b0429c" 
+  },
+  {
+    _id: uuid(),
+    categoryName: "life-hacks",
+    categoryTitle: "Life Hacks",
+    description: "Useful Tricks For All Occasions",
+    categoryImg: "https://firebasestorage.googleapis.com/v0/b/video-library-image-data.appspot.com/o/life-hacks%2Flife1.jpg?alt=media&token=0c32d956-1cdb-450e-a39d-a689d402ce3f" 
   },
 ];
--- src/backend/db/videos.js
+++ src/backend/db/videos.js
@@ -5,17 +5,183 @@
 
 export const videos = [
   {
-    _id: "Wo5dMEP_BbI",
-    title: "Awesome Video about Coding",
-    description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
-    creator: "Soham Shah",
+    _id: "4H-CeCtYwS8",
+    title: "FANTASTIC EXPERIMENTS YOU WILL WANT TO TRY",
+    description: "Amazing tricks and fun experiments you can repeat at home",
+    creator: "5-Minute Crafts",
+    categoryName: "science-experiments",
+    creatorLogo: "https://firebasestorage.googleapis.com/v0/b/video-library-image-data.appspot.com/o/logo1.jpg?alt=media&token=bcdc5037-cead-4866-91e5-fe738d9b66b3",
+    thumbnail: "https://firebasestorage.googleapis.com/v0/b/video-library-image-data.appspot.com/o/Science-craft%2Fscience1.jpg?alt=media&token=5d86ea5a-51ad-4633-ae9f-14fc5309ccff"
   },
   {
-    _id: "F_Riqjdh2oM",
-    title: "Neural Networks from Scratch - P.1 Intro and Neuron Code",
-    creator: "Sentdex",
-    description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+    _id: "SiD7HS7j9hk",
+    title: "WOW! 🤯 FANTASTIC EXPERIMENTS THAT WILL BLOW YOUR MIND",
+    description: "Awesome hacks and science experiments",
+    creator: "5-Minute Crafts",
+    categoryName: "science-experiments",
+    creatorLogo: "https://firebasestorage.googleapis.com/v0/b/video-library-image-data.appspot.com/o/logo1.jpg?alt=media&token=bcdc5037-cead-4866-91e5-fe738d9b66b3",
+    thumbnail: "https://firebasestorage.googleapis.com/v0/b/video-library-image-data.appspot.com/o/Science-craft%2Fscience2.jpg?alt=media&token=b00673c5-356e-496e-9546-b5e59277051b"
+  },
+  {
+    _id: "bBK3n_HY340",
+    title: "26 UNBELIEVABLE WATER TRICKS",
+    description: "GENIUS WATER HACKS AND TRICKS THAT WILL MAKE YOU FEEL INSPIRED",
+    creator: "5-Minute Crafts",
+    categoryName: "science-experiments",
+    creatorLogo: "https://firebasestorage.googleapis.com/v0/b/video-library-image-data.appspot.com/o/logo1.jpg?alt=media&token=bcdc5037-cead-4866-91e5-fe738d9b66b3",
+    thumbnail: "https://firebasestorage.googleapis.com/v0/b/video-library-image-data.appspot.com/o/Science-craft%2Fscience3.jpg?alt=media&token=47dea228-c09a-4713-8907-0cc696829099"
+  },
+  {
+    _id: "rJRWrvuwb3A",
+    title: "20 ABSOLUTELY BEAUTIFUL DIYs",
+    description: "COLORFUL IDEAS AND EXPERIMENTS YOU SHOULD TRY",
+    creator: "5-Minute Crafts",
+    categoryName: "science-experiments",
+    creatorLogo: "https://firebasestorage.googleapis.com/v0/b/video-library-image-data.appspot.com/o/logo1.jpg?alt=media&token=bcdc5037-cead-4866-91e5-fe738d9b66b3",
+    thumbnail: "https://firebasestorage.googleapis.com/v0/b/video-library-image-data.appspot.com/o/Science-craft%2Fscience4.jpg?alt=media&token=61a62966-26d4-49a0-876d-1ec753fcef60"
+  },
+  {
+    _id: "xctlaTmJ_zQ",
+    title: "22 CRAZY HOME SCIENCE TRICKS YOU'VE NEVER SEEN",
+    description: "SCIENCE EXPERIMENTS FOR KIDS AND ADULTS",
+    creator: "5-Minute Crafts",
+    categoryName: "science-experiments",
+    creatorLogo: "https://firebasestorage.googleapis.com/v0/b/video-library-image-data.appspot.com/o/logo1.jpg?alt=media&token=bcdc5037-cead-4866-91e5-fe738d9b66b3",
+    thumbnail: "https://firebasestorage.googleapis.com/v0/b/video-library-image-data.appspot.com/o/Science-craft%2Fscience5.jpg?alt=media&token=492f5d48-453b-4d75-9831-cb4ec37a26aa"
+  },
+  {
+    _id: "r3iSRAcqI6Q",
+    title: "23 BRILLIANT PHONE HACKS",
+    description: "SMATRPHONE HACKS EVERYBODY IS GOING TO LOVE",
+    creator: "5-Minute Crafts",
+    categoryName: "craft",
+    creatorLogo: "https://firebasestorage.googleapis.com/v0/b/video-library-image-data.appspot.com/o/logo1.jpg?alt=media&token=bcdc5037-cead-4866-91e5-fe738d9b66b3",
+    thumbnail: "https://firebasestorage.googleapis.com/v0/b/video-library-image-data.appspot.com/o/craft%2Fcraft1.webp?alt=media&token=e83135d6-2cd4-4ad6-a008-e118c3a773d8"
+  },
+  {
+    _id: "Hh0aenK67hA",
+    title: "25 HOT GLUE HACKS AND CRAFTS",
+    description: "GRAB YOUR HOT GLUE GUN!",
+    creator: "5-Minute Crafts",
+    categoryName: "craft",
+    creatorLogo: "https://firebasestorage.googleapis.com/v0/b/video-library-image-data.appspot.com/o/logo1.jpg?alt=media&token=bcdc5037-cead-4866-91e5-fe738d9b66b3",
+    thumbnail: "https://firebasestorage.googleapis.com/v0/b/video-library-image-data.appspot.com/o/craft%2Fcraft2.webp?alt=media&token=ab6cf585-1958-417f-bd3e-c27644ee8f8d"
+  },
+  {
+    _id: "F7WME9PGc10",
+    title: "20 BRIGHT CRAFTING HACKS THAT WILL CHANGE YOUR LIFE",
+    description: "BRIGHT CRAFTING HACKS",
+    creator: "5-Minute Crafts",
+    categoryName: "craft",
+    creatorLogo: "https://firebasestorage.googleapis.com/v0/b/video-library-image-data.appspot.com/o/logo1.jpg?alt=media&token=bcdc5037-cead-4866-91e5-fe738d9b66b3",
+    thumbnail: "https://firebasestorage.googleapis.com/v0/b/video-library-image-data.appspot.com/o/craft%2Fcraft3.webp?alt=media&token=3fe266b8-7ae1-44c6-9292-b611662ea352"
+  },
+  {
+    _id: "zhKPdPS48tY",
+    title: "ONE MINUTE DRAWING LESSON",
+    description: "A brilliant drawing trick that will help you become a better artist.",
+    creator: "5-Minute Crafts",
+    categoryName: "craft",
+    creatorLogo: "https://firebasestorage.googleapis.com/v0/b/video-library-image-data.appspot.com/o/logo1.jpg?alt=media&token=bcdc5037-cead-4866-91e5-fe738d9b66b3",
+    thumbnail: "https://firebasestorage.googleapis.com/v0/b/video-library-image-data.appspot.com/o/craft%2Fcraft4.jpg?alt=media&token=beba56d8-0459-4057-89aa-aa2bb9012fa8"
+  },
+  {
+    _id: "CcdgO10VbcQ",
+    title: "25 EASY GIFT WRAPPING IDEAS AND HACKS",
+    description: "5-Minute Crafts KIDS",
+    creator: "5-Minute Crafts",
+    categoryName: "craft",
+    creatorLogo: "https://firebasestorage.googleapis.com/v0/b/video-library-image-data.appspot.com/o/logo1.jpg?alt=media&token=bcdc5037-cead-4866-91e5-fe738d9b66b3",
+    thumbnail: "https://firebasestorage.googleapis.com/v0/b/video-library-image-data.appspot.com/o/craft%2Fcraft5.webp?alt=media&token=978b60a2-db7f-4737-bd7a-6f4f579d07a4"
+  },
+  {
+    _id: "XUPbVof7uwo",
+    title: "WHAT TO DO ON A BORING DAY || 26 FUN ART IDEAS AND DOODLES",
+    description: "COOL WAYS TO ENTERTAIN YOURSELF",
+    creator: "5-Minute Crafts",
+    categoryName: "smart-parenting-hacks",
+    creatorLogo: "https://firebasestorage.googleapis.com/v0/b/video-library-image-data.appspot.com/o/logo1.jpg?alt=media&token=bcdc5037-cead-4866-91e5-fe738d9b66b3",
+    thumbnail: "https://firebasestorage.googleapis.com/v0/b/video-library-image-data.appspot.com/o/smart-parenting-hacks%2Fparenting1.webp?alt=media&token=4478835a-95c6-4b0d-898a-82d2e6b0429c"
+  },
+  {
+    _id: "O-0pycMJwxU",
+    title: "Amazing Kid's Room Makeover || Guide For Parents",
+    description: "Upgrade your kid's room with this excellent guide",
+    creator: "5-Minute Crafts",
+    categoryName: "smart-parenting-hacks",
+    creatorLogo: "https://firebasestorage.googleapis.com/v0/b/video-library-image-data.appspot.com/o/logo1.jpg?alt=media&token=bcdc5037-cead-4866-91e5-fe738d9b66b3",
+    thumbnail: "https://firebasestorage.googleapis.com/v0/b/video-library-image-data.appspot.com/o/smart-parenting-hacks%2Fparenting2.webp?alt=media&token=8edd0d57-f49e-4324-8dde-a1409dd0fcc6"
+  },
+  {
+    _id: "FcG45_dpQmI",
+    title: "SMART PARENTING HACKS AND CRAFTS",
+    description: "Incredible parenting ideas to help you when the time comes.",
+    creator: "5-Minute Crafts",
+    categoryName: "smart-parenting-hacks",
+    creatorLogo: "https://firebasestorage.googleapis.com/v0/b/video-library-image-data.appspot.com/o/logo1.jpg?alt=media&token=bcdc5037-cead-4866-91e5-fe738d9b66b3",
+    thumbnail: "https://firebasestorage.googleapis.com/v0/b/video-library-image-data.appspot.com/o/smart-parenting-hacks%2Fparenting3.webp?alt=media&token=fc2244ef-a5e2-4046-bcc7-18999136665f"
+  },
+  {
+    _id: "WjjCsvgODeg",
+    title: "LIFE HACKS FOR PARENTS || Travel and Camping Gadgets for Smart Moms and Dads",
+    description: "There's the great feeding spoon, the chair which attaches to your suitcase and many more.",
+    creator: "5-Minute Crafts",
+    categoryName: "smart-parenting-hacks",
+    creatorLogo: "https://firebasestorage.googleapis.com/v0/b/video-library-image-data.appspot.com/o/logo1.jpg?alt=media&token=bcdc5037-cead-4866-91e5-fe738d9b66b3",
+    thumbnail: "https://firebasestorage.googleapis.com/v0/b/video-library-image-data.appspot.com/o/smart-parenting-hacks%2Fparenting4.webp?alt=media&token=66b275e3-77d1-4638-b3b5-9264267ec969"
+  },
+  {
+    _id: "bfLd-QJdp1s",
+    title: "31 SMART PARENTING HACKS AND GADGETS || DIY Clothes Ideas And Easy Activities Every Mom Should Know",
+    description: "Parenting DIYs and genius gadgets every mom and dad need to have.",
+    creator: "5-Minute Crafts",
+    categoryName: "smart-parenting-hacks",
+    creatorLogo: "https://firebasestorage.googleapis.com/v0/b/video-library-image-data.appspot.com/o/logo1.jpg?alt=media&token=bcdc5037-cead-4866-91e5-fe738d9b66b3",
+    thumbnail: "https://firebasestorage.googleapis.com/v0/b/video-library-image-data.appspot.com/o/smart-parenting-hacks%2Fparenting5.webp?alt=media&token=61dbb690-4d66-478e-8d0b-7398ba529eaf"
+  },
+  {
+    _id: "jgbLx4HI_hk",
+    title: "RANDOM HACKS THAT WILL MAKE YOUR LIFE EASIER || Useful Tricks For All Occasions",
+    description: "Hi everyone! In this video, we gathered many helpful hacks for different occasions. If you're a parent, you will find some valuable ideas too.",
+    creator: "5-Minute Crafts",
+    categoryName: "life-hacks",
+    creatorLogo: "https://firebasestorage.googleapis.com/v0/b/video-library-image-data.appspot.com/o/logo1.jpg?alt=media&token=bcdc5037-cead-4866-91e5-fe738d9b66b3",
+    thumbnail: "https://firebasestorage.googleapis.com/v0/b/video-library-image-data.appspot.com/o/life-hacks%2Flife1.jpg?alt=media&token=0c32d956-1cdb-450e-a39d-a689d402ce3f"
+  },
+  {
+    _id: "xWZa_X_zS2g",
+    title: "GENIUS HACKS FOR PET OWNERS || DIY IDEAS AND GADGETS YOU SHOULD SEE",
+    description: "Hi, friends! In this video we prepared lots of genius hacks, crafts and gadgets for your pets.",
+    creator: "5-Minute Crafts",
+    categoryName: "life-hacks",
+    creatorLogo: "https://firebasestorage.googleapis.com/v0/b/video-library-image-data.appspot.com/o/logo1.jpg?alt=media&token=bcdc5037-cead-4866-91e5-fe738d9b66b3",
+    thumbnail: "https://firebasestorage.googleapis.com/v0/b/video-library-image-data.appspot.com/o/life-hacks%2Flife2.jpg?alt=media&token=f0e115a3-0ff0-4b1d-95aa-5a6f4272024b"
+  },
+  {
+    _id: "8ap0SNZ8AWk",
+    title: "GENIUS HACKS & GADGETS FOR SMART PET OWNERS",
+    description: "Hello, smart pet owners! In this video, we will share with you a fresh portion of hacks, DIYs, and gadgets for your lovely pets.",
+    creator: "5-Minute Crafts",
+    categoryName: "life-hacks",
+    creatorLogo: "https://firebasestorage.googleapis.com/v0/b/video-library-image-data.appspot.com/o/logo1.jpg?alt=media&token=bcdc5037-cead-4866-91e5-fe738d9b66b3",
+    thumbnail: "https://firebasestorage.googleapis.com/v0/b/video-library-image-data.appspot.com/o/life-hacks%2Flife3.jpg?alt=media&token=0394b91d-4c65-4b71-a588-145c45b6b041"
+  },
+  {
+    _id: "NrqTRJH4xRw",
+    title: "SMART FIRST-AID HACKS EVERYONE SHOULD KNOW",
+    description: "Hello everyone! In this video we will share with you mix of emergency hacks and first aid tips that will make your life easier.",
+    creator: "5-Minute Crafts",
+    categoryName: "life-hacks",
+    creatorLogo: "https://firebasestorage.googleapis.com/v0/b/video-library-image-data.appspot.com/o/logo1.jpg?alt=media&token=bcdc5037-cead-4866-91e5-fe738d9b66b3",
+    thumbnail: "https://firebasestorage.googleapis.com/v0/b/video-library-image-data.appspot.com/o/life-hacks%2Flife4.jpg?alt=media&token=110d7091-0391-450f-bed4-072c7240fbdc"
+  },
+  {
+    _id: "ptltadl4IJw",
+    title: "SMART ORGANIZING & MOVING HACKS YOU SHOULD TRY",
+    description: "Hi everyone! In this video, we will share with you the best moving hacks and organizing tips that will help you keep your place clean.",
+    creator: "5-Minute Crafts",
+    categoryName: "life-hacks",
+    creatorLogo: "https://firebasestorage.googleapis.com/v0/b/video-library-image-data.appspot.com/o/logo1.jpg?alt=media&token=bcdc5037-cead-4866-91e5-fe738d9b66b3",
+    thumbnail: "https://firebasestorage.googleapis.com/v0/b/video-library-image-data.appspot.com/o/life-hacks%2Flife5.jpg?alt=media&token=e7738951-74d3-4dae-9aad-ea8a33b6a90d"
   },
 ];
